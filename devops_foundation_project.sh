d7ceffa389fbad8ba2760408e754058194b3e657
AV1970 <42776808+AV1970@users.noreply.github.com>
2024-02-13 17:00:33 +0000

    Update devops_foundation_project.sh

--- devops_foundation_project.sh
+++ devops_foundation_project.sh
@@ -1,7 +1,7 @@
 # Create a Full Backup usinc rsync and compression
 
 # Edited crontab using crontab -e and add the line to run fullbackup.sh everyday at 02AM
-# 0 * * * /home/myuser/full_backup.sh
+# 0 2 * * * /home/myuser/full_backup.sh
 
 # Create a compressed tar file with full backup of the folder python3_code
 tar -zcvf fullbackup.tar.gz python3_code
